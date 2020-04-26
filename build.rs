6c2816790ff27443e23524e576b27f261ff919bc
chip <chip@chip.sh>
2020-04-26 00:14:27 -0700

    set cmake generator for windows msvc as recommended

--- build.rs
+++ build.rs
@@ -1,3 +1,13 @@
 fn main() {
-    cmake::build("flatbuffers");
+    let mut config = cmake::Config::new("flatbuffers");
+
+    // specially set the visual studio generator version on msvc windows
+    // this generator is recommended by https://google.github.io/flatbuffers/flatbuffers_guide_building.html
+    /*    let target = std::env::var("TARGET").unwrap();
+    let host = std::env::var("HOST").unwrap();
+    if target.contains("msvc") && host.contains("windows") {
+        config.generator("Visual Studio 15 2017");
+    }*/
+
+    config.build();
 }
