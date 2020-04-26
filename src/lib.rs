744a7f01d64451149116a6f241564aa42fe13213
chip <chip@chip.sh>
2020-04-26 01:17:42 -0700

    add docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,6 @@
+#[forbid(unsafe_code)]
+
+/// Path of the built `flatc` executable.
 pub fn flatc() -> &'static std::path::Path {
     std::path::Path::new(concat!(env!("OUT_DIR"), "/bin/flatc"))
 }
