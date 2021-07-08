a63f32a1226ec2e9536c8222352cde83070d6f7c
Justin Wernick <justin@worthe-it.co.za>
2021-07-08 13:18:48 +0200

    Add a CXX flag that the code said it needed
    When I compiled before on MSVC, the warning was as follows:
    
    "warning C4530: C++ exception handler used, but unwind semantics are
    not enabled. Specify /EHsc"
    
    That sounds reasonable, but it seems strange to me that this isn't
    handled by the flatbuffers CMakeLists.txt. Also, I don't see this
    documented at all in the flatbuffers repo or used in their CI. I don't
    generally work on windows, so I don't know if this is just something
    that every windows dev knows.

--- build.rs
+++ build.rs
@@ -1,3 +1,11 @@
 fn main() {
-    cmake::build("flatbuffers");
+    let mut config = cmake::Config::new("flatbuffers");
+
+    let target = std::env::var("TARGET").unwrap();
+    let host = std::env::var("HOST").unwrap();
+    if target.contains("msvc") && host.contains("windows") {
+        config.cxxflag("/EHsc");
+    }
+
+    config.build();
 }
